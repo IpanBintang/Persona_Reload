f7e11e496fecfa1ceb8f78618fa73f8d635864c2
IpanBintang <2024629932@student.uitm.edu.my>
2026-04-10 02:17:16 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Persona_Reload/',
-  root: 'src',          // ← add this
   build: {
-    outDir: '../dist',  // ← and this, so dist stays at root
+    outDir: 'dist',
   },
 })
